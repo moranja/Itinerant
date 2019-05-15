ad9bb445ed53e9b549b17ee4e7ce4da6b01fca52
moranja <jeffadammoran@gmail.com>
2019-05-15 17:28:06 -0500

    cors and fetch testing

--- backend/app/controllers/attractions_controller.rb
+++ backend/app/controllers/attractions_controller.rb
@@ -1,2 +1,5 @@
 class AttractionsController < ApplicationController
+  def index
+    render json: Attraction.all
+  end
 end
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
 import React from 'react'
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch("http://localhost:3000/attractions")
+    .then(res => res.json())
+    .then(res => console.log(res))
+  }
+  
   render() {
     return (
       <div>

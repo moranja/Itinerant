c90788e69b8d8ba165ba5f7e5c0eb7098cc2978e
moranja <jeffadammoran@gmail.com>
2019-05-16 15:12:21 -0500

    oops

--- frontend/src/components/MyMapComponent.js
+++ frontend/src/components/MyMapComponent.js
@@ -5,7 +5,7 @@ import SearchBox from "react-google-maps/lib/components/places/SearchBox"
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAALXKRDZu3lDYTuMUMxXWCqq3CRm5dZrU&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=???&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,

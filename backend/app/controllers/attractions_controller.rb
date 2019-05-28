025f2ccb43119a0c406d2823fbb959f9afd8c3b0
moranja <jeffadammoran@gmail.com>
2019-05-28 17:24:06 -0500

    edit attraction modal

--- backend/app/controllers/attractions_controller.rb
+++ backend/app/controllers/attractions_controller.rb
@@ -17,6 +17,20 @@ class AttractionsController < ApplicationController
     end
   end
 
+  def update
+    attraction = Attraction.find(params[:id])
+
+    if attraction.area.city.itinerary.users.map{|u| u.id}.include?(@current_user.id)
+      attraction.update(name: params[:name], classification: params[:classification], description: params[:description])
+      render json: attraction.area.city.itinerary.full_itinerary
+    else
+      render json: {
+        error: true,
+        message: 'You do not have permission to edit this itinerary'
+      }
+    end
+  end
+
   private
 
     def attraction_params
--- frontend/src/components/Attraction.js
+++ frontend/src/components/Attraction.js
@@ -1,9 +1,67 @@
 import React from 'react'
+import EditAttractionModal from './EditAttractionModal'
+import { connect } from 'react-redux'
 
-const Attraction = (props) => (
-  <React.Fragment>
-    <li>{`${props.name} (${props.classification}): ${props.description}`}</li>
-  </React.Fragment>
+const mapStateToProps = (state) => ({
+  itinerary: state.selected_itinerary
+})
+
+const mapDispatchToProps = {
+  editAttraction: attraction_info => dispatch => {
+    const editArea = {
+      attraction_id: attraction_info.attractionId,
+      name: attraction_info.name,
+      classification: attraction_info.classification,
+      description: attraction_info.description
+    }
+    fetch(`http://localhost:3000/attractions/${attraction_info.attractionId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${localStorage.token}`
+      },
+      body: JSON.stringify({
+        ...editArea
+      })
+    })
+    .then(res => res.json())
+    .then(res => {
+      if (res.error) {
+        console.log(res.message)
+      } else {
+        dispatch({ type: "LOAD_SELECTED_ITINERARY", payload: res})
+      }
+    })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (
+  class Attraction extends React.Component {
+    state = {
+      editName: this.props.name,
+      editClassification: this.props.classification,
+      editDescription: this.props.description
+    }
+
+    handleChange = (e) => {
+      this.setState({[e.target.id]: e.target.value})
+    }
+
+    handleEditSubmit = () => {
+      let payload = {
+        attractionId: this.props.id,
+        name: this.state.editName,
+        classification: this.state.editClassification,
+        description: this.state.editDescription
+      }
+      this.props.editAttraction(payload)
+    }
+
+
+    render() {
+      return (
+        <EditAttractionModal handleChange={this.handleChange} handleEditSubmit={this.handleEditSubmit} {...this.state} />
+      )
+    }
+  }
 )
-
-export default Attraction
